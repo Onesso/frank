7e86e74cfbf7d9d5248d480fb9d754612002233a
Onesso <odhiambofrank1965@gmail.com>
2025-07-14 08:52:48 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faInstagram,
+  faWhatsapp,
+  faXTwitter,
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'frank-were';
+  faInstagram = faInstagram;
+  faWhatsapp = faWhatsapp;
+  faXTwitter = faXTwitter;
+
+  public openWhatsApp() {
+    const phone = '+254115984633';
+    const message = encodeURIComponent('Hello, Frank i need you to..');
+    window.open(`https://wa.me/${phone}?text=${message}`, '_blank');
+  }
 }
